ab0bc10f51e8fb4dc587131689dce94ce51d4613
EzeqCM <rpcnet02@gmail.com>
2022-06-24 18:15:54 -0400

    Atualizando

---
+++ Downloads/prova_web_18/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='tudo'>
+    <div className='divisaolado'>
+
+      <div className='fundo'>
+      <button>Processos</button>
+      <p>Processos relacionados ao combate do   COVID-19</p>
+      </div>
+      
+      <div className='fundo'>
+      <button>Contratos</button>
+      <p>Contratos relacionados ao combate do   COVID-19</p>
+      </div>
+
+    </div>
+    
+    <div className='linhareta'>
+    <button>Licitações e Compras Diretas (Dispensas)</button>
+      <p>Compras destinadas ao enfrentamento e combate a COVID-19</p>
+    </div>
+
+    
+    <div className='divisaolado'>
+
+      <div className='fundo'>
+      <button>Receitas Federais</button>
+      <p>Processos relacionados ao combate do   COVID-19</p>
+      </div>
+      
+      <div className='fundo'>
+      <button>Receitas Estaduais</button>
+      <p>Contratos relacionados ao combate do   COVID-19</p>
+      </div>
+
+    </div>
+
+    <div className='divisaolado'>
+      <div className='fundo'>
+      <button>Empenhos</button>
+      <p>Processos relacionados ao combate do   COVID-19</p>
+      </div>
+      
+      <div className='fundo'>
+      <button>Liquidações</button>
+      <p>Contratos relacionados ao combate do   COVID-19</p>
+      </div>
+
+      <div className='fundo'>
+      <button>Pagamentos</button>
+      <p>Contratos relacionados ao combate do   COVID-19</p>
+      </div>
+
+    </div>
+
+    </div>
+    
+    
+  )
+}
+
+export default App
---
+++ Downloads/prova_web_18/src/VerMais.jsx
@@ -0,0 +1,7 @@
+function VerMais(){
+    
+    return(
+        <p><a className='VerMais' href="#" onclick="myFunction();">Mostrar Mais</a></p>
+    );
+}
+export default VerMais;
